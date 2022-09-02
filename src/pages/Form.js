28db04cd1edc5cc037162c90224dad63e5e05c5b
BadgersetGuy <86418490+BadgersetGuy@users.noreply.github.com>
2022-09-02 13:27:11 +0300

    create form, add validations

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -3,8 +3,19 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
+
 const FormComponent = () => {
     let navigate = useNavigate();
+
+    const submitForm = (values) => {
+        // first set the values in local storage
+        localStorage.setItem("data", JSON.stringify(values));
+        
+        // then set the values in redux
+        
+        // redirect to the home page
+        navigate("../", { replace: true });
+    };
 
     // if the user is not logged in, redirect to the login page
     useEffect(() => {
@@ -17,14 +28,11 @@ const FormComponent = () => {
     })
 
     
-
-
         return (
   
             <div>
-                <h1>Any place in your app!</h1>
                 <Formik
-                initialValues={{ email: '', password: '' }}
+                initialValues={{ email: '', dob: '' }}
                 validate={values => {
                     const errors = {};
                     if (!values.email) {
@@ -34,21 +42,33 @@ const FormComponent = () => {
                     ) {
                     errors.email = 'Invalid email address';
                     }
+
+                    if (!values.dob) {
+                        errors.date = 'Required';
+                    } else if (
+                        !/^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/.test(values.dob)
+                    ) {
+                    errors.dob = 'Invalid date';
+                    }
+
                     return errors;
                 }}
                 onSubmit={(values, { setSubmitting }) => {
                     setTimeout(() => {
                     alert(JSON.stringify(values, null, 2));
+
+                    submitForm(values);
+
                     setSubmitting(false);
                     }, 400);
                 }}
                 >
                 {({ isSubmitting }) => (
-                    <Form>
+                    <Form className="bg-slate-50 p-8">
                     <Field type="email" name="email" />
                     <ErrorMessage name="email" component="div" />
-                    <Field type="password" name="password" />
-                    <ErrorMessage name="password" component="div" />
+                    <Field type="date" name="dob" />
+                    <ErrorMessage name="dob" component="div" />
                     <button type="submit" disabled={isSubmitting}>
                         Submit
                     </button>
